b4632d1d23f8e508a12c53c51c0c176d3a7db051
cerimonialbabr <cerimonial.babr@gmail.com>
2026-08-01 13:08:45 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.DASHBOARD_CONFIG = {
-  API_URL: "SUBSTITUIR_PELA_URL_DO_WEB_APP",
+  API_URL: "https://script.google.com/macros/s/AKfycbzYfL02rvQCjFcC3poHdbbl4DjJ2fbVZbfIrsgOSnuwdoVWTW5KpkI1ZpCCvhBu0C4yYg/exec",
   REFRESH_MINUTES: 3,
   USE_DEMO_WHEN_UNCONFIGURED: true
 };
